8be11e04929e9946fb19a85e77245f664da46309
ilyass <ilyass.bentalha2@gmail.com>
2024-10-02 23:03:06 +0100

    add files

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,53 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, durationInSeconds) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', durationInSeconds, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+}
+
+export default new RedisClient();
